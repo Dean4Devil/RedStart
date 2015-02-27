e624962097a25cdd021caa15e23bbcff7862f141
Dean4Devil <dean4devil@chaingamers.com>
2015-02-27 15:25:56 +0100

    FIrst API commit. Changing the config actually does something now

--- src/redstart/api/mod.rs
+++ src/redstart/api/mod.rs
@@ -1,3 +1,21 @@
 //! This module contains all the API sub-modules that are useable by RedStart
+use configreader::ConfigReader;
+use redstart::session::Store;
 
-pub struct API;
+pub struct API
+{
+    pub config: ConfigReader,
+    pub sessions: Store,
+}
+
+impl API
+{
+    pub fn new() -> API
+    {
+        API
+        {
+            config: ConfigReader::new(),
+            sessions: Store::new(),
+        }
+    }
+}
