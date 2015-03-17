0d09d9b2181595eb816b83b0998ba4e9a31ba06e
Dean4Devil <dean4devil@chaingamers.com>
2015-03-17 19:58:19 +0100

    Added HTTPS capabilities

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ extern crate toml;
 extern crate cookie;
 
 //use std::error::Error;
+use std::os;
+use std::path::PathBuf;
 use std::old_io::net::ip::{SocketAddr, IpAddr};
 
 use iron::prelude::*;
@@ -54,7 +56,17 @@ fn main()
     let addr: IpAddr = api.config.get_value_or::<String>("Networking.address", "localhost".to_string()).parse().unwrap();
     let port = api.config.get_value_or::<u16>("Networking.port", 3000);
     let sock_addr = SocketAddr { ip: addr, port: port };
-	Iron::new(chain).http(sock_addr).unwrap();
+
+    let mut cert_path = os::self_exe_path().unwrap();
+    let mut key_path = cert_path.clone();
+    let cert_conf = api.config.get_value_or::<String>("Security.certificate", "snakeoil.key".to_string());
+    cert_path.push(cert_conf);
+    let key_conf = api.config.get_value_or::<String>("Security.key", "snakeoil.cert".to_string());
+    key_path.push(key_conf);
+
+    println!("{}", key_path.as_str().unwrap());
+
+	Iron::new(chain).https(sock_addr, cert_path, key_path).unwrap();
 }
 
 #[cfg(test)]
