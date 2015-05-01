6816ff7a1ee0d671dddd3c62b07b107d157ffead
Dean4Devil <dean4devil@chaingamers.com>
2015-05-01 15:37:01 +0200

    Added MySQL functionality to the API

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,5 +1,7 @@
 //This Module contains various Traits and implementations for Data Storage.
 
+pub use self::mysql::MySQL;
+
 // Traits
 pub mod crud;
 
--- src/data/mysql.rs
+++ src/data/mysql.rs
@@ -1,30 +1,37 @@
 //! MySQL Driver implementation
-use mysql::conn::MyOpts;
+use mysql::conn::{MyOpts, pool};
 
+#[derive(Clone)]
 pub struct MySQL
 {
-    opts: MyOpts,
+    pub pool: pool::MyPool,
+}
+
+fn get_opts() -> MyOpts 
+{
+    MyOpts
+    {
+        user: Some("root".to_string()),
+        pass: Some("DidRPwfMySQL".to_string()),
+        db_name: Some("reservator".to_string()),
+        tcp_port: 3306,
+        tcp_addr: Some("127.0.0.1".to_string()),
+        unix_addr: None,
+        prefer_socket: true,
+        init: vec![],
+        verify_peer: false,
+        ssl_opts: None,
+    }
 }
 
 impl MySQL
 {
     pub fn new() -> MySQL
     {
+        let opts = get_opts();
         MySQL
         {
-            opts: MyOpts
-            {
-                user: Some("root".to_string()),
-                pass: Some("DidRPwfMySQL".to_string()),
-                db_name: Some("reservator".to_string()),
-                tcp_port: 3306,
-                tcp_addr: Some("127.0.0.1".to_string()),
-                unix_addr: None,
-                prefer_socket: true,
-                init: vec![],
-                verify_peer: false,
-                ssl_opts: None,
-            },
+            pool: pool::MyPool::new(opts).unwrap(),
         }
     }
 }
