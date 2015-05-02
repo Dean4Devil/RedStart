54e3422ab97ba7e22593e3e5555aa8ec5785a042
Dean4Devil <dean4devil@chaingamers.com>
2015-05-02 18:58:39 +0200

    Implemented group/list
    This has to be tested!

--- src/controller/group.rs
+++ src/controller/group.rs
@@ -7,6 +7,10 @@
 
 use std::io::Read;
 
+use serialize::json;
+use serialize::Encodable;
+use serialize::json::{ToJson,Encoder};
+
 use iron::prelude::*;
 use iron::status::{self, Status};
 
@@ -25,12 +29,14 @@ impl Group
 
     pub fn call(&self, model: &str, req: &mut Request) -> Response
     {
+        let mut list = List::new(self.api.ggnet.clone());
+
         let body: Box<Read + Send>;
         let (status, body) = match model
         {
             "list" =>
             {
-                (status::NotImplemented, "".to_string())
+                list.call(req)
             },
             _ =>
             {
@@ -39,5 +45,26 @@ impl Group
         };
 
         Response::new().set(status).set(body)
+    }
+}
+
+struct List
+{
+    ggnet: GGNet,
+}
+
+impl List
+{
+    pub fn new(ggnet: GGNet) -> List
+    {
+        List { ggnet: ggnet }
+    }
+
+    pub fn call(&mut self, req: &mut Request) -> (Status, String)
+    {
+        let res_vec = self.ggnet.get_groups("*");
+        let res_json = res_vec.to_json();
+
+        (status::Ok, json::encode(&res_vec).unwrap())
     }
 }
